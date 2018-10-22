10567b3f1c01ace878f512432cff50e11d78cf6b
Oscar Bautista <oscar.bautista123@gmail.com>
2018-10-22 01:00:46 -0400

    Initial commit

---
+++ app/src/main/java/obautista/uga/edu/statecapitalsquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package obautista.uga.edu.statecapitalsquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
