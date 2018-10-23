058dd2e4d266cd90761271edc01383ee12d486dd
NYDanielLim <lim.daniel.10@gmail.com>
2018-10-23 02:21:56 -0400

    Added the main activities
    Added History, Main, and Quiz acitivities. Need to finally get the sliding feature implemented.

---
+++ app/src/main/java/obautista/uga/edu/statecapitalsquiz/HistoryActivity.java
@@ -0,0 +1,19 @@
+package obautista.uga.edu.statecapitalsquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HistoryActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_activity);
+
+    }
+}
+
--- app/src/main/java/obautista/uga/edu/statecapitalsquiz/MainActivity.java
+++ app/src/main/java/obautista/uga/edu/statecapitalsquiz/MainActivity.java
@@ -1,13 +1,39 @@
 package obautista.uga.edu.statecapitalsquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button quizzesBtn;
+    private Button historyBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quizzesBtn=(Button) findViewById(R.id.quizBtn);
+        historyBtn=(Button) findViewById(R.id.historyBtn);
+
+
+        quizzesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
